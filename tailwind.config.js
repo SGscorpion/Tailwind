090dde9744004c70acb750d8876115e7bbdc0b6e
SGscorpion <sohanpokhrel57@gmail.com>
2024-05-19 11:59:01 +0545

    cv

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/index.html","./dist/index1.html"],
+  content: ["./dist/index.html","./dist/index1.html","./dist/sudam.html"],
   theme: {
     extend: {},
   },
